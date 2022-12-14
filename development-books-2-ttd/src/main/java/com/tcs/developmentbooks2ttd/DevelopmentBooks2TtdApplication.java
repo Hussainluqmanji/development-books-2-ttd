f7b44ec8d1d21242b961da10ab96287c6a953d40
HussainLuqmanji <hluqmanji@gmail.com>
2022-12-14 20:00:05 +0530

    feat: create springboot project for development books.

---
+++ development-books-2-ttd/src/main/java/com/tcs/developmentbooks2ttd/DevelopmentBooks2TtdApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.developmentbooks2ttd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentBooks2TtdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentBooks2TtdApplication.class, args);
+	}
+
+}
