fbd4e9b774964b467196b70d0c1b064fc2627422
HussainLuqmanji <hluqmanji@gmail.com>
2022-12-15 13:11:32 +0530

    feat: book details added in enum along with name.

--- development-books-2-ttd/src/test/java/com/tcs/developmentbooks2ttd/enums/BooksEnumTest.java
+++ development-books-2-ttd/src/test/java/com/tcs/developmentbooks2ttd/enums/BooksEnumTest.java
@@ -10,4 +10,14 @@ public class BooksEnumTest {
 	public void booksEnumShouldContainFiveDevelopmentBooks() {
 		assertEquals(5, BooksEnum.values().length);
 	}
+
+	@Test
+	public void booksShouldHaveAllTheDetails() {
+		BooksEnum cleanCodeBook = BooksEnum.CLEAN_CODE;
+		assertEquals(BooksEnum.CLEAN_CODE.getId(), cleanCodeBook.getId());
+		assertEquals(BooksEnum.CLEAN_CODE.getAuthor(), cleanCodeBook.getAuthor());
+		assertEquals(BooksEnum.CLEAN_CODE.getTitle(), cleanCodeBook.getTitle());
+		assertEquals(BooksEnum.CLEAN_CODE.getYear(), cleanCodeBook.getYear());
+		assertEquals(BooksEnum.CLEAN_CODE.getPrice(), cleanCodeBook.getPrice(), 0.0);
+	}
 }
